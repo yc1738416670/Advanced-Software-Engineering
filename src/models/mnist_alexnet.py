8b390d7a09343a99a88d9767bd0b2c5477e95938
yc1738416670 <77599492+yc1738416670@users.noreply.github.com>
2024-05-13 20:24:36 +0800

    Update mnist_alexnet.py

--- src/models/mnist_alexnet.py
+++ src/models/mnist_alexnet.py
@@ -1,1 +1,36 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+class Mnist_AlexNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=5, stride=1, padding=2)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, stride=1, padding=2)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=5, stride=1, padding=2)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        
+        self.fc1 = nn.Linear(4*4*256, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 10)
+
+    def forward(self, inputs):
+        tensor = inputs.view(-1, 1, 28, 28)
+        tensor = F.relu(self.bn1(self.conv1(tensor)))
+        tensor = self.pool1(tensor)
+        tensor = F.relu(self.bn2(self.conv2(tensor)))
+        tensor = self.pool2(tensor)
+        tensor = F.relu(self.bn3(self.conv3(tensor)))
+        tensor = self.pool3(tensor)
+        tensor = tensor.view(-1, 4*4*256)
+        tensor = F.relu(self.fc1(tensor))
+        tensor = F.relu(self.fc2(tensor))
+        tensor = self.fc3(tensor)
+        return tensor
